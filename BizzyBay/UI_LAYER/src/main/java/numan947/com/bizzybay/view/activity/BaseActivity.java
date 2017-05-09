4a8dbe89f7e1a13afdfc1265412e4db0f4ef1a27
numan947 <mahmudulhasan947@gmail.com>
2017-05-09 20:24:56 +0600

    Changed translucent/transparent status bar setting style and moved it to java code, Added Collapsing Toolbar layout in the details product activity

--- BizzyBay/UI_LAYER/src/main/java/numan947/com/bizzybay/view/activity/BaseActivity.java
+++ BizzyBay/UI_LAYER/src/main/java/numan947/com/bizzybay/view/activity/BaseActivity.java
@@ -1,6 +1,15 @@
 package numan947.com.bizzybay.view.activity;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import numan947.com.bizzybay.R;
 
 /**
  *
@@ -9,8 +18,38 @@ import android.support.v7.app.AppCompatActivity;
  *
  * Base Class for all the Activities.
  * Extend this class for implementing more activities.
+ * Basically this activity overrides the {@code onCreate()} method and
+ * sets up translucent status bar.
  *
  **/
 
 public abstract class BaseActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        if(window!=null){
+            if( Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP)
+                setTranslucentStatusBarLollipop(window);
+            else if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT)
+                setTranslucentStatusBarKitkat(window);
+        }
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void setTranslucentStatusBarLollipop(Window window){
+        window.setStatusBarColor(ContextCompat.getColor(window.getContext(),R.color.transparent));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void setTranslucentStatusBarKitkat(Window window)
+    {
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    }
+
+
 }
